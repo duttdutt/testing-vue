8596012beba6fe20eb55ecb4f19254afcc2b5781
duttdutt <kartashov.developer@gmail.com>
2024-09-21 01:42:24 +0300

    added router

---
+++ src/router.ts
@@ -0,0 +1,17 @@
+// router.ts
+import { createRouter, createMemoryHistory } from 'vue-router';
+import View from './views/View.vue';
+import HomeView from './views/HomeView.vue';
+import AboutView from './views/AboutView.vue';
+
+const routes = [
+  { path: '/:page', component: View, children: [
+    { path: 'home', component: HomeView },
+    { path: 'about', component: AboutView },
+  ]},
+];
+
+export const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
