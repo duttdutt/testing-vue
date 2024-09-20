1cd00d426ce47d320819f48799d46754cbcece0c
ynk <ynk@ynks-MacBook-Air.local>
2024-09-21 01:43:55 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts">
+<script setup>
+import { ref, reactive } from 'vue';
+
+const image = ref(0);
+const change = () => image.value++;
 </script>
 
 <template>
+    <button @click="change">{{ image }}</button>
 </template>
 
-<style>
+<style scoped>
 </style>
